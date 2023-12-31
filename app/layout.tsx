c44c3273a97b5f3a0aadb54a516df2516a1b57e1
Matthew <matthew.godwin.3954@gmail.com>
2023-12-31 13:33:16 +0100

    Fixing app height

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={`${inter.className} w-full h-full`}>
+                <body className={`${inter.className}`}>
                     <ThemeProvider
                         attribute="class"
                         defaultTheme="system"
